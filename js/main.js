23d78da0998460781c07520a3bac2826b1a5e0e3
Hayley McDonald <hhmcdonald@gmail.com>
2016-06-10 18:45:24 -0500

    Add objects based on 5 US presidents

--- js/main.js
+++ js/main.js
@@ -4,3 +4,43 @@ var iceCream = [
     'Cookies n Cream',
     'Cake Batter'
 ];
+
+var Obama = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: '2 terms',
+    party: 'Democratic',
+    yearsOfPresidency: '8 years'
+};
+
+var BushJr = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: '2 terms',
+    party: 'Republican',
+    yearsOfPresidency: '8 years'
+};
+
+var Clinton = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '2 terms',
+    party: 'Democratic',
+    yearsOfPresidency: '8 years'
+};
+
+var BushSr = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: '1 term',
+    party: 'Republican',
+    yearsOfPresidency: '4 years'
+};
+
+var Reagan = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: '2 terms',
+    party: 'Republican',
+    yearsOfPresidency: '8 years'
+};
