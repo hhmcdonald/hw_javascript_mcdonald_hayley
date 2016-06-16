fc13fc127db83af9b216b46718b4fbe87360b6fc
Richard Kalehoff <richardkalehoff@gmail.com>
2016-06-15 22:46:56 -0400

    indent array code correctly

--- js/main.js
+++ js/main.js
@@ -7,45 +7,45 @@ var iceCream = [
 
 var presidents = [
 
-{
-    firstName: 'Barack',
-    lastName: 'Obama',
-    termLength: '2 terms',
-    party: 'Democratic',
-    yearsOfPresidency: '8 years'
-},
+    {
+        firstName: 'Barack',
+        lastName: 'Obama',
+        termLength: '2 terms',
+        party: 'Democratic',
+        yearsOfPresidency: '8 years'
+    },
 
-{
-    firstName: 'George',
-    lastName: 'Bush',
-    termLength: '2 terms',
-    party: 'Republican',
-    yearsOfPresidency: '8 years'
-},
+    {
+        firstName: 'George',
+        lastName: 'Bush',
+        termLength: '2 terms',
+        party: 'Republican',
+        yearsOfPresidency: '8 years'
+    },
 
-{
-    firstName: 'Bill',
-    lastName: 'Clinton',
-    termLength: '2 terms',
-    party: 'Democratic',
-    yearsOfPresidency: '8 years'
-},
+    {
+        firstName: 'Bill',
+        lastName: 'Clinton',
+        termLength: '2 terms',
+        party: 'Democratic',
+        yearsOfPresidency: '8 years'
+    },
 
-{
-    firstName: 'George',
-    lastName: 'Bush',
-    termLength: '1 term',
-    party: 'Republican',
-    yearsOfPresidency: '4 years'
-},
+    {
+        firstName: 'George',
+        lastName: 'Bush',
+        termLength: '1 term',
+        party: 'Republican',
+        yearsOfPresidency: '4 years'
+    },
 
-{
-    firstName: 'Ronald',
-    lastName: 'Reagan',
-    termLength: '2 terms',
-    party: 'Republican',
-    yearsOfPresidency: '8 years'
-},
+    {
+        firstName: 'Ronald',
+        lastName: 'Reagan',
+        termLength: '2 terms',
+        party: 'Republican',
+        yearsOfPresidency: '8 years'
+    },
 
 console.log( presidents[3].firstName + presidents[3].lastName )
 
